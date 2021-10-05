6439a37b63722c01914e4f049c6286c6e60e1fb5
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-10-05 10:24:08 -0700

    questions, answers form, timer

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var insultAPI = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
 // what api to use for reward?
 
+var timeLeft = 10;
+var downloadTimer = setInterval(function(){
+    if (timeLeft <=0) {
+        clearInterval(downloadTimer);
+        document.getElementById("seconds-left").innerHTML = "next question";
+    } else {
+        document.getElementById("seconds-left").innerHTML = timeLeft;
+    }
+    timeLeft -= 1;
+}, 1000);
