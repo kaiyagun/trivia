b6881ced4b845bb0b7d2b2ec87e92dbae6a42c35
Jonathan Singh <jmsingh9899@gmail.com>
2021-10-05 10:01:00 -0700

    making profile page

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,10 @@ var insultAPI = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
 // what api to use for reward?
 
+var profileLoad = function () {
+    var profilePage = $("#profile");
+    console.log(profilePage)
+
+}
+
+profileLoad()
