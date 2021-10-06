6bfff806b2c036cc3d2a4b7687008abf6a3d4be6
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-10-06 11:30:31 -0700

    timer starts on generate btn, added function for wronganswer and linked it to timer whwn timer =0

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 yeet 
+
+
+//pseudocode overview
+//on load: trivia-gen card is visible, choose paramaters for game, pulls available parameters from localstorage (based on past scores)
+//gen button clikc: calls show question (pulls random uestion from api based on chosen parameters)
+//on clicking answer: flips card
+//on card flip: shows correct answer, reward/insult, and adds to score
+//also shows next question button, which calls displayquestion again 
+//repeat until end of quiz
+//quiz end: prompt for name, saves score and parameter data to localstorage
+
+//on profile load: pulls local storage info and orgasnizes based on page
+//loads name (from former prompt) and assigns data to the name
+
